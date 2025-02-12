3557f987da8a586b1d4fa316de1f244d6d67a0de
Josef Gabrielsson <josef.gabrielsson@outlook.com>
2025-02-12 14:43:09 +0100

    fix buges

--- src/App.js
+++ src/App.js
@@ -3,45 +3,55 @@ import './App.css'
 
 function App() {
   const [display, setDisplay] = useState('0')
+  const [reset, setReset] = useState(false)
 
-  const handleClick = (val) => {
+  const handleInput = (val) => {
     setDisplay(!val ? '0' : (prev => (prev === '0' ? '' : prev) + val))
+    if (reset) {
+      setDisplay(val)
+      setReset(false)
+    }
   }
 
   const handleCalc = () => {
-    try{
+    try {
       setDisplay(eval(display))
-    }catch(e) {
+      setReset(true)
+    } catch (e) {
       setDisplay('ERROR?')
     }
+  }
+
+  const handleReset = () => {
+    setDisplay(0);
   }
 
   return (
     <div className="App">
       <input type="text" value={display} readOnly></input>
 
-      <button class="first" onClick={() => handleClick()}>AC</button>
-      <button class="first" onClick={() => handleClick(7)}>7</button>
-      <button class="first" onClick={() => handleClick(4)}>4</button>
-      <button class="first" onClick={() => handleClick(1)}>1</button>
+      <button class="first" onClick={() => handleReset()}>AC</button>
+      <button class="first" onClick={() => handleInput(7)}>7</button>
+      <button class="first" onClick={() => handleInput(4)}>4</button>
+      <button class="first" onClick={() => handleInput(1)}>1</button>
       <button class="first"></button>
 
       <button class="second"></button>
-      <button class="second" onClick={() => handleClick(8)}>8</button>
-      <button class="second" onClick={() => handleClick(5)}>5</button>
-      <button class="second" onClick={() => handleClick(2)}>2</button>
+      <button class="second" onClick={() => handleInput(8)}>8</button>
+      <button class="second" onClick={() => handleInput(5)}>5</button>
+      <button class="second" onClick={() => handleInput(2)}>2</button>
       <button class="second"></button>
 
       <button class="third"></button>
-      <button class="third" onClick={() => handleClick(9)}>9</button>
-      <button class="third" onClick={() => handleClick(6)}>6</button>
-      <button class="third" onClick={() => handleClick(3)}>3</button>
+      <button class="third" onClick={() => handleInput(9)}>9</button>
+      <button class="third" onClick={() => handleInput(6)}>6</button>
+      <button class="third" onClick={() => handleInput(3)}>3</button>
       <button class="third"></button>
 
-      <button class="last" onClick={() => handleClick('/')}>÷</button>
-      <button class="last" onClick={() => handleClick('*')}>×</button>
-      <button class="last" onClick={() => handleClick('-')}>-</button>
-      <button class="last" onClick={() => handleClick('+')}>+</button>
+      <button class="last" onClick={() => handleInput('/')}>÷</button>
+      <button class="last" onClick={() => handleInput('*')}>×</button>
+      <button class="last" onClick={() => handleInput('-')}>-</button>
+      <button class="last" onClick={() => handleInput('+')}>+</button>
       <button class="last" onClick={() => handleCalc()}>=</button>
     </div>
   );
